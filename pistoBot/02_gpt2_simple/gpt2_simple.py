a4c9968b889b84ff1093c0f796fe429d3183a428
Simone Guardati <simone.guardati42@gmail.com>
2020-04-25 12:21:43 +0200

    - [gpt2-simple] local parameter - [gpt2-simple] log input info

--- pistoBot/02_gpt2_simple/gpt2_simple.py
+++ pistoBot/02_gpt2_simple/gpt2_simple.py
@@ -19,6 +19,7 @@ def run(path_params: str, local: bool):
     params_ml = params['ml']
     params_gen = params['generation']
 
+    logging.info(f"Input file {params_data['file_path']} have {len(params_data['file_path'])}")
     # Init
     gpt2.download_gpt2(model_name="124M")
 
