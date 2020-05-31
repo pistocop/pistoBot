0943ce8eb8e31736eb83e6a5d06b93cd542a10e9
GuardatiSimone <simone.guardati42@gmail.com>
2020-05-31 11:58:46 +0200

    - [gpt2scratch] tokenizer completed

--- pistoBot/03_gpt2_scratch/gpt2_scratch.py
+++ pistoBot/03_gpt2_scratch/gpt2_scratch.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import yaml
 import logging
@@ -5,6 +6,11 @@ import argparse
 from os import makedirs
 from os.path import basename, normpath, join
 from datetime import datetime
+
+from aitextgen.TokenDataset import TokenDataset
+from aitextgen.tokenizers import train_tokenizer
+from aitextgen.utils import GPT2ConfigCPU
+from aitextgen import aitextgen
 
 sys.path.append("./")  # needed 4 utils imports - created according to launcher
 from pistoBot.utils.general_utils import my_init, load_yaml
@@ -20,54 +26,25 @@ def run(path_params: str):
 
     # Init
     timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    model_dir = params_ml['save_path']
-    model_name = params_ml['model_size']  # Used from GPT2 to check if model is '117M' or '124M'
-    run_name = f"02_gp2simple_{params_ml['model_size']}_{timestamp}"
+    run_name = f"03_gpt2scratch_{timestamp}"
+    model_dir = join(params_ml['save_path'], run_name)
+    os.makedirs(model_dir, exist_ok=True)
 
-    gpt2.download_gpt2(model_name=model_name, model_dir=model_dir)
-
-    # Fine-tune
-    sess = gpt2.start_tf_sess()
-
-    gpt2.finetune(sess,
-                  model_dir=model_dir,
-                  model_name=model_name,
-                  checkpoint_dir=model_dir,
-                  run_name=run_name,
-                  dataset=params_data['file_path'],
-                  steps=params_ml['steps'],
-                  learning_rate=params_ml['learning_rate'],
-                  restore_from=params_ml['restore_from'],
-                  print_every=params_ml['print_every'],
-                  sample_every=params_ml['sample_every'],
-                  save_every=params_ml['save_every'])
-
-    # Generate
-    text_generated = gpt2.generate(sess,
-                                   run_name=run_name,
-                                   model_dir=model_dir,
-                                   model_name=model_name,
-                                   prefix=params_gen['prefix'],
-                                   temperature=params_gen['temperature'],
-                                   return_as_list=True)
-
-    # Output persist
-    model_params_path = join(model_dir, 'gpt2_simple_params.yaml')
-    with open(model_params_path, 'w') as f:
-        yaml.dump(params, f, default_flow_style=False)
-    logging.debug(f"Model params saved at {model_params_path}")
-
-    makedirs(join(model_dir, run_name, 'text_generated'), exist_ok=True)
-    text_generated_path = join(model_dir, run_name, 'text_generated', f'{timestamp}.txt')
-    open(text_generated_path, 'w').writelines('\n'.join(text_generated))
-
-    logging.debug(f"Text generated saved at {text_generated_path} - {len(text_generated)} total lines")
+    # Train tokenizer
+    logging.info("Training tokenizer...")
+    dropout = params_ml['tokenizer_dropout'] if params_ml['tokenizer_dropout'] != 0.0 else None
+    train_tokenizer(files=params_data['file_path'],
+                    dropout=dropout,
+                    vocab_size=params_ml['vocab_size'],
+                    min_frequency=params_ml['min_frequency'],
+                    save_path=model_dir)
+    logging.info("Training tokenizer completed!")
 
 
 def main(argv):
     parser = argparse.ArgumentParser(prog=argv[0])
     parser.add_argument("--path_params", help="Path to rnn YAML params",
-                        default="./pistoBot/03_gpt2_scratch/gpt2_simple_params.yaml")
+                        default="./pistoBot/03_gpt2_scratch/gpt2_scratch_params.yaml")
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     args = parser.parse_args(argv[1:])
     loglevel = logging.DEBUG if args.verbose else logging.INFO
